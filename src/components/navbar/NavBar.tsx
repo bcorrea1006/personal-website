e1756daad098b880e178eebd37e3a245d8d910bd
bcorrea1006 <benitoscorrea@gmail.com>
2025-01-04 10:35:48 -0800

    navbar draft

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,5 +1,25 @@
+import { HStack } from '@chakra-ui/react';
+import RightContent from './RightContent';
+
 const NavBar = () => {
-  return <h1>navbar</h1>;
+  return (
+    <HStack
+      width='full'
+      alignItems='center'
+      justifyContent='space-between'
+      p={4}
+      bg='red.emphasized'
+    >
+      {/* left content */}
+      <h1>BC</h1>
+
+      {/* right content */}
+      <RightContent />
+
+      {/* Middle Content */}
+      <h1>Toggle</h1>
+    </HStack>
+  );
 };
 
 export default NavBar;
---
+++ src/components/navbar/RightContent.tsx
@@ -0,0 +1,13 @@
+import { Button, Flex } from '@chakra-ui/react';
+
+const RightContent = () => {
+  return (
+    <Flex>
+      <Button>About</Button>
+      <Button>Portfolio</Button>
+      <Button>Contacts</Button>
+    </Flex>
+  );
+};
+
+export default RightContent;
