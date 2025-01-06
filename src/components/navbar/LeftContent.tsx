0a5bfa790661b3d1335b2793bdf07b6dbca04525
bcorrea1006 <benitoscorrea@gmail.com>
2025-01-05 19:46:30 -0800

    added me profile pic :3

--- src/components/navbar/LeftContent.tsx
+++ src/components/navbar/LeftContent.tsx
@@ -1,8 +1,14 @@
 import { Button, Flex, Heading, HStack, Link } from '@chakra-ui/react';
+import { Avatar } from '../ui/avatar';
 
 const LeftContent = () => {
   return (
     <Flex alignItems='center' justifyContent='space-between' gap={4}>
+      <Avatar
+        size='2xl'
+        name='Benito Correa'
+        src='../src/assets/profile_pic.webp'
+      />
       <Heading color='teal' fontWeight='black' size='5xl'>
         BC
       </Heading>
